d75d05ad98af482f21aa36f5da52e0c756b6d926
SlayerWx <maxi_cs_95@hotmail.com>
2022-03-20 00:47:40 -0300

    Test script

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using CustomMath;
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("vector3: " + -new Vector3(-1f, -1f, -1f));
+        // Debug.Log("vec3:    " + -new Vec3(1f, 1f, 1f));
+    }
+}
