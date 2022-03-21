d4be8a28554748962be3a87c2c9abae1fbf3b925
SlayerWx <maxi_cs_95@hotmail.com>
2022-03-21 10:09:59 -0300

    debugger

--- Assets/ejercicios/21del3/ejer1.cs
+++ Assets/ejercicios/21del3/ejer1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ejer1 : MonoBehaviour
+public class Ejer1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
