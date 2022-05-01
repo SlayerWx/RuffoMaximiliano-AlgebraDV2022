14e7f4b66b9108c191529debbf1ec3185f291bb2
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-05-01 07:05:56 -0300

    plano a partir de 3 puntos

--- Assets/Scripts/MyPlane.cs
+++ Assets/Scripts/MyPlane.cs
@@ -7,16 +7,22 @@ namespace CustomPlane
 {
     public class MyPlane : MonoBehaviour
     {
-        public Vec3 point = Vec3.Zero;
         public Vec3 normal = Vec3.Zero;
         public float distance = 0f;
         public MyPlane(Vec3 inPoint,Vec3 inNormal)
         {
 	    //calculo de un plano: normal.a * punto a + normal.b * punto.b + normal.c * punto.c + distancia = 0
             normal = inNormal;
-            point = inPoint;
             distance = -Vec3.Dot(inNormal,inPoint); // distancia al punto que apunta la normal 
-            Debug.Log(distance);
+            Debug.Log("distance: " + distance);
+        }
+        public MyPlane(Vec3 A, Vec3 B, Vec3 C)
+        {
+            // B - A y C - A
+
+            normal = Vec3.Cross(B - A, C - A).normalized; // vectores perpendiculares al plano para sacar la normal a partir
+                                                          // de 3 puntos
+            distance = -Vec3.Dot(normal, A); // distancia al punto que apunta la normal 
         }
         public void Flip()
         {
