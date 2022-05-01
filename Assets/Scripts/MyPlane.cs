8e6f55e57df5de9d23b6f34ae97f2f5c15af369e
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-05-01 06:10:16 -0300

    constructor de plano

--- Assets/Scripts/MyPlane.cs
+++ Assets/Scripts/MyPlane.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MyPlane : MonoBehaviour
+using CustomMath;
+namespace CustomPlane
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MyPlane : MonoBehaviour
     {
-        
-    }
+        Vec3 point;
+        Vec3 normal;
+        float distance;
+        public MyPlane(Vec3 inPoint,Vec3 inNormal)
+        {
+	    //calculo de un plano: normal.a * punto a + normal.b * punto.b + normal.c * punto.c + distancia = 0
+            normal = inNormal;
+            point = inPoint;
+            distance = -Vec3.Dot(inNormal,inPoint); // distancia al punto que apunta la normal 
+            Debug.Log(distance);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Scripts/PlaneTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomMath;
+using CustomPlane;
+public class PlaneTest : MonoBehaviour
+{
+    MyPlane plane;
+    public Vec3 origin;
+    public Vec3 normal;
+    void Start()
+    {
+        plane = new MyPlane(origin, normal);
+    }
+
+}
