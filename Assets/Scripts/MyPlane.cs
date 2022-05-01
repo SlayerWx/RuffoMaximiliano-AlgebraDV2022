1bb446955edb1bac735913f93576a6788c4ce76b
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-05-01 06:20:30 -0300

    set y get variables

--- Assets/Scripts/MyPlane.cs
+++ Assets/Scripts/MyPlane.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using CustomMath;
 namespace CustomPlane
 {
     public class MyPlane : MonoBehaviour
     {
-        Vec3 point;
-        Vec3 normal;
-        float distance;
+        public Vec3 point { set { point = value; } get { return point; } }
+        public Vec3 normal { set { normal = value; } get { return normal; } }
+        public float distance {  set { distance = value; } get { return distance; } }
         public MyPlane(Vec3 inPoint,Vec3 inNormal)
         {
 	    //calculo de un plano: normal.a * punto a + normal.b * punto.b + normal.c * punto.c + distancia = 0
@@ -17,6 +18,7 @@ namespace CustomPlane
             distance = -Vec3.Dot(inNormal,inPoint); // distancia al punto que apunta la normal 
             Debug.Log(distance);
         }
+    
 
-    }
+}
 }
