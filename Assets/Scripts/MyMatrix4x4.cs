9b4541f1825dcbca375bd9f72e0752924df61e8a
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-07-04 03:53:58 -0300

    ToString Matriz 4x4

--- Assets/Scripts/MyMatrix4x4.cs
+++ Assets/Scripts/MyMatrix4x4.cs
@@ -420,6 +420,25 @@ namespace CustomMath
             hashCode = hashCode * -1521134295 + LossyScale.GetHashCode();
             return hashCode;
         }
-
+        public override string ToString()
+        {
+            return (
+             "m00  " + m00 +
+            "  m01  " + m01 +
+            "  m02  " + m02 +
+            "  m03  " + m03 +
+            "\nm10  " + m10 +
+            "  m11  " + m11 +
+            "  m12  " + m12 +
+            "  m13  " + m13 +
+            "\nm20  " + m20 +
+            "  m21  " + m21 +
+            "  m22  " + m22 +
+            "  m23  " + m23 +
+            "\nm30  " + m30 +
+            "  m31  " + m31 +
+            "  m32  " + m32 +
+            "  m33  " + m33);
+        }
     }
 }
